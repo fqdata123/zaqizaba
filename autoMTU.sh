969c55f19a9c434211074a8aa322145146ed2836
fqdata123 <93412694+fqdata123@users.noreply.github.com>
2022-04-06 15:44:12 +0800

    Create autoMTU.sh

---
+++ autoMTU.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+MTU=$((1500-28))
+[[ $IPV4$IPV6 = 01 ]] && ping6 -c1 -W1 -s $MTU -Mdo 2606:4700:d0::a29f:c001 >/dev/null 2>&1 || ping -c1 -W1 -s $MTU -Mdo 162.159.193.10 >/dev/null 2>&1
+until [[ $? = 0 || $MTU -le $((1280+80-28)) ]]
+do
+MTU=$((MTU-10))
+[[ $IPV4$IPV6 = 01 ]] && ping6 -c1 -W1 -s $MTU -Mdo 2606:4700:d0::a29f:c001 >/dev/null 2>&1 || ping -c1 -W1 -s $MTU -Mdo 162.159.193.10 >/dev/null 2>&1
+done
+
+if [[ $MTU -eq $((1500-28)) ]]; then MTU=$MTU
+elif [[ $MTU -le $((1280+80-28)) ]]; then MTU=$((1280+80-28))
+else
+	for ((i=0; i<9; i++)); do
+	(( MTU++ ))
+	( [[ $IPV4$IPV6 = 01 ]] && ping6 -c1 -W1 -s $MTU -Mdo 2606:4700:d0::a29f:c001 >/dev/null 2>&1 || ping -c1 -W1 -s $MTU -Mdo 162.159.193.10 >/dev/null 2>&1 ) || break
+	done
+	(( MTU-- ))
+fi
+
+MTU=$((MTU+28-80))
+
+[[ -e /etc/wireguard/wgcf.conf ]] && sed -i "s/MTU.*/MTU = $MTU/g" /etc/wireguard/wgcf.conf && green " \n${T[${L}81]}\n "
