2fe6a36aae31428a426ba2e09695000f263f59ba
fqdata123 <93412694+fqdata123@users.noreply.github.com>
2022-04-08 14:03:11 +0800

    Update autoMTU.sh

--- autoMTU.sh
+++ autoMTU.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+IPV4=0
+IPV6=1
 MTU=$((1500-28))
 [[ $IPV4$IPV6 = 01 ]] && ping6 -c1 -W1 -s $MTU -Mdo 2606:4700:d0::a29f:c001 >/dev/null 2>&1 || ping -c1 -W1 -s $MTU -Mdo 162.159.193.10 >/dev/null 2>&1
 until [[ $? = 0 || $MTU -le $((1280+80-28)) ]]
